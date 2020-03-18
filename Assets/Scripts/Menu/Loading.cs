b71c654916ecc9d9d37d9f026a91348adb366db9
Denis <zhursky16@gmail.com>
2020-03-19 01:06:13 +0300

    Главное меню
    Добавлена сцена загрузки и сцена главного меню.

---
+++ Assets/Scripts/Menu/Loading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    private Transitions transitions;
+
+    private void Awake()
+    {
+        transitions = GetComponent<Transitions>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(GoToMenu());
+    }
+
+    /// <summary>
+    /// Переход в главное меню
+    /// </summary>
+    private IEnumerator GoToMenu()
+    {
+        yield return new WaitForSeconds(2.0f);
+        transitions.GoToScene(Transitions.Scene.Menu);
+    }
+}
---
+++ Assets/Scripts/Menu/Transitions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transitions : MonoBehaviour
+{
+    public enum Scene { Menu = 1 }
+
+    /// <summary>
+    /// Переход на указанную сцену
+    /// </summary>
+    /// <param name="scene">Сцена для перехода</param>
+    public void GoToScene(Scene scene)
+    {
+        SceneManager.LoadScene((int)scene);
+    }
+
+    public void GoToScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
