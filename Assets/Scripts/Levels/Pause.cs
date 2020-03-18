1fc6a9c0068f4f13e9703bfc2eaa4f2a0cdbc9b3
Denis <zhursky16@gmail.com>
2020-03-18 17:13:42 +0300

    Игровая пауза
    Добавлена игровая пауза, добавлено меню паузы.

---
+++ Assets/Scripts/Levels/Pause.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    /// <summary>
+    /// Настройка игровой паузы
+    /// </summary>
+    /// <param name="state">Состояние паузы</param>
+    public void SetPause(bool state)
+    {
+        Time.timeScale = state ? 0 : 1;
+    }
+}
